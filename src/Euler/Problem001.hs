17cb6f4feb7a9548c0acf1c354e27455524b9e03
Dan Seminara <semi225599@gmail.com>
2015-04-04 15:41:58 -0400

    changes

--- src/Euler/Problem001.hs
+++ src/Euler/Problem001.hs
@@ -2,6 +2,17 @@ module Euler.Problem001
 ( problem1
 ) where
 
+import Data.List.Ordered
 import Euler.Util
 
-problem1 = NoInputI $ sum [a | a <- [1..999], a `mod` 3 == 0 || a `mod` 5 == 0]
+combos xs = map (map product) . tail $ combosBySize xs
+    where combosBySize = foldr f ([[]] : replicate (length xs) [])
+          f x next     = zipWith (++) (map (map (x:)) ([]:next)) next
+
+sumNumsUpToDivisibleBy n ms = sum . concat . zipWith map (cycle [(*1), (*(-1))]) . map (map f) 
+                           . combos $ removeMultiples ms ms
+    where removeMultiples [] ys = ys
+          removeMultiples (x:xs) ys = removeMultiples xs $ ys `minus` [2*x, 3*x ..]
+          f a = (n - n `mod` a) * ((n `div` a) + 1) `div` 2
+
+problem1 = NoInputI $ sumNumsUpToDivisibleBy 999 [3, 5]
