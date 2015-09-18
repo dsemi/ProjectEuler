c2c553cb1066f0d8d46a1c4d4581358525a97624
Dan Seminara <semi225599@gmail.com>
2015-09-18 00:42:28 -0400

    cleanup 93 and fix bug where fractions were always ignored

--- src/Euler/Problem093.hs
+++ src/Euler/Problem093.hs
@@ -2,47 +2,50 @@ module Euler.Problem093
 ( problem93
 ) where
 
-import Data.Char
-import Data.Maybe
 import Control.Arrow
 import Control.Monad
+import Data.Char
 import qualified Data.IntSet as S
 import Data.List (tails, permutations)
+import Data.Maybe
+import Data.Ratio
 import Euler.Util
+
+data Operation = Plus | Minus | Multiply | Divide deriving (Show, Enum)
+data Expr = Const (Ratio Int) | Apply Operation Expr Expr
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _  = [ [] ]
 combinations n xs = [ y:ys | y:xs' <- tails xs
-                    , ys <- combinations (n-1) xs']
+                    , ys <- combinations (n-1) xs' ]
 
-plus :: Int -> Int -> Maybe Int
-a `plus` b = Just $ a+b
-
-minus :: Int -> Int -> Maybe Int
-a `minus` b = Just $ a-b
-
-mult :: Int -> Int -> Maybe Int
-a `mult` b = Just $ a*b
-
-divide :: Int -> Int -> Maybe Int
-a `divide` b
-    | a `mod` b == 0 = Just $ a `div` b
-    | otherwise      = Nothing
-
-operations = combinations 3 $ concatMap (replicate 3) [plus,minus,mult,divide]
-
-apply :: [Int] -> [Int]
-apply nums = let xs = [ S.fromList $ catMaybes [o1, o2, o3, o4, o5] | [a,b,c,d] <- permutations nums
-                      , [p,q,r] <- operations
-                      , let o1 = p a b >>= \x -> q x c >>= \y -> r y d
-                      , let o2 = join $ liftM2 q (p a b) (r c d)
-                      , let o3 = q b c >>= p a >>= \x -> r x d
-                      , let o4 = q b c >>= \x -> r x d >>= p a
-                      , let o5 = r c d >>= q b >>= p a ]
-             in S.toAscList $ S.unions xs
+eval :: Expr -> Maybe (Ratio Int)
+eval (Const n)                  = Just n
+eval (Apply Plus     exp1 exp2) = (+) <$> eval exp1 <*> eval exp2
+eval (Apply Minus    exp1 exp2) = (-) <$> eval exp1 <*> eval exp2
+eval (Apply Multiply exp1 exp2) = (*) <$> eval exp1 <*> eval exp2
+eval (Apply Divide   exp1 exp2) = join $ divide <$> eval exp1 <*> eval exp2
+    where divide a b
+              | b /= 0    = Just $ a / b
+              | otherwise = Nothing
 
 numConsec :: [Int] -> Int
-numConsec = length . takeWhile id . zipWith (==) [1..] . dropWhile (<=0)
+numConsec = length . takeWhile id . zipWith (==) [1..]
+            . dropWhile (< 1) . S.toAscList . S.fromList . f
+    where f nums = catMaybes [ numerator <$> res | ns <- permutations $ map (% 1) nums
+                             , let [a, b, c, d] = map Const ns
+                             , let ops = [ Plus .. ]
+                             , i <- [0..63]
+                             , let (op1, op2, op3) = ( ops !! (i `quot` 16 `rem` 4)
+                                                     , ops !! (i `quot` 4 `rem` 4)
+                                                     , ops !! (i `rem` 4) )
+                             , tree <- [ Apply op3 (Apply op2 (Apply op1 a b) c) d
+                                       , Apply op2 (Apply op1 a b) $ Apply op3 c d
+                                       , Apply op3 (Apply op1 a $ Apply op2 b c) d
+                                       , Apply op1 a $ Apply op3 (Apply op2 b c) d
+                                       , Apply op1 a . Apply op2 b $ Apply op3 c d ]
+                             , let res = eval tree
+                             , liftM denominator res == Just 1 ]
 
-problem93 = NoInput . map intToDigit . snd . maximum 
-            . map (numConsec . apply &&& id) $ combinations 4 [1..9]
+problem93 = NoInput . map intToDigit . snd . maximum
+            . map (numConsec &&& id) $ combinations 4 [1..9]
