b0d5a2b3be0612f63652a5c467d7c812e56f5b49
Dan Seminara <semi225599@gmail.com>
2015-09-25 12:17:29 -0400

    minor changes to 93

--- src/Euler/Problem093.hs
+++ src/Euler/Problem093.hs
@@ -2,16 +2,18 @@ module Euler.Problem093
 ( problem93
 ) where
 
+import Euler.Util
+
 import Control.Arrow
 import Control.Monad
 import Data.Char
 import qualified Data.IntSet as S
+import Data.Ix
 import Data.List (tails, permutations)
 import Data.Maybe
-import Euler.Util
 
-data Operation = Plus | Minus | Multiply | Divide
-data Expr = Const Int | Apply Operation Expr Expr
+data Operation = (:+:) | (:-:) | (:*:) | (:/:) deriving (Eq, Ord, Ix)
+data Expr = Const Int | Apply Expr Operation Expr
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _  = [ [] ]
@@ -19,30 +21,28 @@ combinations n xs = [ y:ys | y:xs' <- tails xs
                     , ys <- combinations (n-1) xs' ]
 
 eval :: Expr -> Double
-eval (Const n)                  = fromIntegral n
-eval (Apply Plus     exp1 exp2) = eval exp1 + eval exp2
-eval (Apply Minus    exp1 exp2) = eval exp1 - eval exp2
-eval (Apply Multiply exp1 exp2) = eval exp1 * eval exp2
-eval (Apply Divide   exp1 exp2) = eval exp1 / eval exp2
+eval (Const n)               = fromIntegral n
+eval (Apply exp1 (:+:) exp2) = eval exp1 + eval exp2
+eval (Apply exp1 (:-:) exp2) = eval exp1 - eval exp2
+eval (Apply exp1 (:*:) exp2) = eval exp1 * eval exp2
+eval (Apply exp1 (:/:) exp2) = eval exp1 / eval exp2
 
-numConsec :: [Int] -> Int
-numConsec = length . takeWhile id . zipWith (==) [1..]
-            . dropWhile (< 1) . S.toAscList . S.fromList . f
+numConsecGT :: Int -> [Int] -> Int
+numConsecGT n = length . takeWhile id . zipWith (==) [1..]
+                . S.toAscList . S.fromList . f
     where f nums = [ res | ns <- permutations nums
                    , let [a, b, c, d] = map Const ns
-                   , let ops = [ Plus, Minus, Multiply, Divide ]
-                   , i <- [0..63]
-                   , let (op1, op2, op3) = ( ops !! (i `quot` 16 `rem` 4)
-                                           , ops !! (i `quot` 4 `rem` 4)
-                                           , ops !! (i `rem` 4) )
-                   , tree <- [ Apply op3 (Apply op2 (Apply op1 a b) c) d
-                             , Apply op2 (Apply op1 a b) $ Apply op3 c d
-                             , Apply op3 (Apply op1 a $ Apply op2 b c) d
-                             , Apply op1 a $ Apply op3 (Apply op2 b c) d
-                             , Apply op1 a . Apply op2 b $ Apply op3 c d ]
+                   , (op1, op2, op3) <- range ( ((:+:), (:+:), (:+:))
+                                              , ((:/:), (:/:), (:/:)))
+                   , tree <- [ Apply (Apply (Apply a op1 b) op2 c) op3 d
+                             , Apply (Apply a op1 b) op2 (Apply c op3 d)
+                             , Apply (Apply a op1 (Apply b op2 c)) op3 d
+                             , Apply a op1 (Apply (Apply b op2 c) op3 d)
+                             , Apply a op1 (Apply b op2 (Apply c op3 d)) ]
                    , let approxRes = eval tree
                    , let res = round approxRes
-                   , abs (fromIntegral res - approxRes) < 0.0001 ]
+                   , abs (fromIntegral res - approxRes) < 0.0001
+                   , res >= n ]
 
 problem93 = NoInput . map intToDigit . snd . maximum
-            . map (numConsec &&& id) $ combinations 4 [1..9]
+            . map (numConsecGT 1 &&& id) $ combinations 4 [1..9]
