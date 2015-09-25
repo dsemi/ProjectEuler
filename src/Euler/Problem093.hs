db57f4838a2c366f4dddc4e59a0e02a501c51147
Dan Seminara <semi225599@gmail.com>
2015-09-25 13:03:14 -0400

    over3

--- src/Euler/Problem093.hs
+++ src/Euler/Problem093.hs
@@ -13,36 +13,33 @@ import Data.List (tails, permutations)
 import Data.Maybe
 
 data Operation = (:+:) | (:-:) | (:*:) | (:/:) deriving (Eq, Ord, Ix)
-data Expr = Const Int | Apply Expr Operation Expr
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _  = [ [] ]
 combinations n xs = [ y:ys | y:xs' <- tails xs
                     , ys <- combinations (n-1) xs' ]
 
-eval :: Expr -> Double
-eval (Const n)               = fromIntegral n
-eval (Apply exp1 (:+:) exp2) = eval exp1 + eval exp2
-eval (Apply exp1 (:-:) exp2) = eval exp1 - eval exp2
-eval (Apply exp1 (:*:) exp2) = eval exp1 * eval exp2
-eval (Apply exp1 (:/:) exp2) = eval exp1 / eval exp2
+toF (:+:) = (+)
+toF (:-:) = (-)
+toF (:*:) = (*)
+toF (:/:) = (/)
 
 numConsecGT :: Int -> [Int] -> Int
-numConsecGT n = length . takeWhile id . zipWith (==) [1..]
+numConsecGT n = length . takeWhile id . zipWith (==) [ n+1 ..]
                 . S.toAscList . S.fromList . f
     where f nums = [ res | ns <- permutations nums
-                   , let [a, b, c, d] = map Const ns
-                   , (op1, op2, op3) <- range ( ((:+:), (:+:), (:+:))
-                                              , ((:/:), (:/:), (:/:)))
-                   , tree <- [ Apply (Apply (Apply a op1 b) op2 c) op3 d
-                             , Apply (Apply a op1 b) op2 (Apply c op3 d)
-                             , Apply (Apply a op1 (Apply b op2 c)) op3 d
-                             , Apply a op1 (Apply (Apply b op2 c) op3 d)
-                             , Apply a op1 (Apply b op2 (Apply c op3 d)) ]
-                   , let approxRes = eval tree
+                   , let [a, b, c, d] = map fromIntegral ns
+                   , (op1, op2, op3) <- map (over3 toF)
+                                        $ range ( ((:+:), (:+:), (:+:))
+                                                , ((:/:), (:/:), (:/:)))
+                   , approxRes <- [ ((a `op1` b) `op2` c) `op3` d
+                                  , (a `op1` b) `op2` (c `op3` d)
+                                  , (a `op1` (b `op2` c)) `op3` d
+                                  , a `op1` ((b `op2` c) `op3` d)
+                                  , a `op1` (b `op2` (c `op3` d)) ]
                    , let res = round approxRes
                    , abs (fromIntegral res - approxRes) < 0.0001
-                   , res >= n ]
+                   , res > n ]
 
 problem93 = NoInput . map intToDigit . snd . maximum
-            . map (numConsecGT 1 &&& id) $ combinations 4 [1..9]
+            . map (numConsecGT 0 &&& id) $ combinations 4 [1..9]
--- src/Euler/Util.hs
+++ src/Euler/Util.hs
@@ -1,6 +1,8 @@
-module Euler.Util
-where
+module Euler.Util where
 
-data Problem a = NoInput String 
+data Problem a = NoInput String
                | HasInput (String -> String)
                | HasRando (Int, Int) ([Int] -> String)
+
+over3 :: (a -> b) -> (a, a, a) -> (b, b, b)
+over3 f (a, b, c) = (f a, f b, f c)
