6cc07fb813b113db0347b4c0eb36869518b51dbd
Dan Seminara <semi225599@gmail.com>
2020-04-23 20:47:52 -0400

    Faster fib

--- src/Euler/Problem025.hs
+++ src/Euler/Problem025.hs
@@ -4,9 +4,4 @@ module Euler.Problem025
 
 import Euler.Util
 
-fibonacci = go 0 1
-    where go :: Integer -> Integer -> Integer -> Integer
-          go a b 0 = a
-          go a b n = go b (a+b) (n-1)
-
-problem25 = NoInput . show $ head [x | x <- [1..], let f = fibonacci x, f `div` 10^999 >= 1]
+problem25 = NoInput . show $ head [x | x <- [1..], fibonacci x `div` 10^999 >= 1]
--- src/Euler/Util.hs
+++ src/Euler/Util.hs
@@ -1,6 +1,8 @@
 module Euler.Util where
 
+import Data.Semigroup
 import System.Random
+
 
 data Problem = NoInput String
              | HasInput (String -> String)
@@ -8,3 +10,18 @@ data Problem = NoInput String
 
 over3 :: (a -> b) -> (a, a, a) -> (b, b, b)
 over3 f (a, b, c) = (f a, f b, f c)
+
+data Matrix = Matrix { x00 :: !Integer, x01 :: !Integer
+                     , x10 :: !Integer, x11 :: !Integer } deriving (Show)
+
+instance Semigroup Matrix where
+    Matrix l00 l01 l10 l11 <> Matrix r00 r01 r10 r11 =
+        Matrix { x00 = l00 * r00 + l01 * r10, x01 = l00 * r01 + l01 * r11
+               , x10 = l10 * r00 + l11 * r10, x11 = l10 * r01 + l11 * r11 }
+
+instance Monoid Matrix where
+    mempty = Matrix 1 0 0 1
+
+fibonacci :: Integer -> Integer
+fibonacci n = x01 $ mtimesDefault n matrix
+    where matrix = Matrix 0 1 1 1
